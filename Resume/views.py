da998fca07be2f81689c3f1d16742b7034f7287d
Pradeep Nishad <47804516+hack000025@users.noreply.github.com>
2021-10-05 14:40:28 +0530

    Update views.py

--- Resume/views.py
+++ Resume/views.py
@@ -14,7 +14,7 @@ from rest_framework import viewsets
 
 class ProfileListCreateAPIView (ListCreateAPIView ):
     # authentication_classes = [JSONWebTokenAuthentication ,]
-    authentication_classes =[BasicAuthentication,]
+#     authentication_classes =[BasicAuthentication,]
     permission_classes = [IsAuthenticated,]
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
@@ -30,7 +30,7 @@ class ProfileListCreateAPIView (ListCreateAPIView ):
 class ProfileDestroyAPIView(DestroyAPIView):
     def delete(self, request, id):
 #         permission_classes = [IsAdminUser,]
-        authentication_classes =[BasicAuthentication,]
+#         authentication_classes =[BasicAuthentication,]
         idno = models.Profile.objects.filter(id=id)
         if len(idno)==0:
             return Response({'message':'Profile not found'}, status=400)
@@ -39,7 +39,7 @@ class ProfileDestroyAPIView(DestroyAPIView):
 
 
 class EducationAPIView(APIView):
-    authentication_classes =[BasicAuthentication,]
+#     authentication_classes =[BasicAuthentication,]
 #     permission_classes = [IsAdminUser,]
     def get(self,request,*args,**kwarg):
         qs = Education.objects.all()
@@ -48,7 +48,7 @@ class EducationAPIView(APIView):
 
 
 class ExperienceAPIView(APIView):
-    authentication_classes =[BasicAuthentication,]
+#     authentication_classes =[BasicAuthentication,]
     def get(self,request,*args,**kwarg):
         qs = Experience.objects.all()
         serializer = ExperienceSerializer(qs,many = True)
@@ -57,7 +57,7 @@ class ExperienceAPIView(APIView):
 
 
 class ProjectsAPIView(APIView):
-    authentication_classes =[BasicAuthentication,]
+#     authentication_classes =[BasicAuthentication,]
     def get(self,request,*args,**kwarg):
         qs = Projects.objects.all()
         serializer = ProjectSerializer(qs,many = True)
@@ -66,14 +66,14 @@ class ProjectsAPIView(APIView):
 
 
 class CertificationAPIView(APIView):
-    authentication_classes =[BasicAuthentication,]
+#     authentication_classes =[BasicAuthentication,]
     def get(self,request,*args,**kwarg):
         qs = Certification.objects.all()
         serializer = CertificationSerializer(qs,many = True)
         return Response(serializer.data,status=200)
 
 class SkillsAPIView(APIView):
-    authentication_classes =[BasicAuthentication,]
+#     authentication_classes =[BasicAuthentication,]
     def get(self,request,*args,**kwargs):
         qs = Skills.objects.all()
         serializer = SkillsSerializers(qs,many = True)
